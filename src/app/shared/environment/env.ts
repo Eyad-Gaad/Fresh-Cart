3c81336214b854a4a9087624a8efd812dad6017d
Eyad-Gaad <eyadgaad10@gmail.com>
2025-02-28 14:05:34 +0200

    update the hosting url

--- src/app/shared/environment/env.ts
+++ src/app/shared/environment/env.ts
@@ -1,5 +1,5 @@
 // shared environment 
 export let env = {
     baseUrl : 'https://ecommerce.routemisr.com',
-    hosting:'http://localhost:4200'
+    hosting:'https://fresh-cart-delta-two.vercel.app'
 }
